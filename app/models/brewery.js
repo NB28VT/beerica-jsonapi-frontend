8fa416923f924696773b54bbd8eaa4c602729e10
Nathan <Burgess@Nathans-MacBook-Pro.local>
2016-01-02 21:24:10 -0500

    Better font and brewery page tweaks yeahhhh

--- app/models/brewery.js
+++ app/models/brewery.js
@@ -4,6 +4,20 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   address: DS.attr('string'),
   state: DS.belongsTo('state'),
+  
+  city: DS.attr('string'),
+  postal_code: DS.attr('string'),
+  latitude: DS.attr('string'),
+  longitude: DS.attr('string'),
+  open_to_public: DS.attr('boolean'),
+  description: DS.attr('string'),
+  website: DS.attr('string'),
+  established: DS.attr('string'),
+  icon_image: DS.attr('string'),
+  medium_image: DS.attr('string'),
+  large_image: DS.attr('string'),
+  square_medium_image: DS.attr('string'),
+  square_large_image: DS.attr('string'),
   beers: DS.hasMany('beer'),
   reviews: DS.hasMany('review')
 });
