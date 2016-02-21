210d97c3fc08051bfbd8cfa0544366cf45d874f7
Nathan <Burgess@Nathans-MacBook-Pro.local>
2016-02-21 17:03:20 -0500

    Beers page

--- app/models/beer.js
+++ app/models/beer.js
@@ -3,5 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   abv: DS.attr('string'),  
+  medium_label: DS.attr('string'),  
+  large_label: DS.attr('string'),  
+  icon: DS.attr('string'),
   brewery: DS.belongsTo('brewery')
 });
